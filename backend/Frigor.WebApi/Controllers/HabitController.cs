be5665f7ff14acfc74429a0feed09810be614d73
Colthouse <colthouse@gmx.net>
2025-08-22 23:48:06 +0200

    feat(backend): added example controller

---
+++ backend/Frigor.WebApi/Controllers/HabitController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Frigor.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HabitController : ControllerBase
+{
+    /// <summary>Get Habits</summary>
+    /// <return>Habits</return>
+    /// <response code="200">Successful</response>
+    [HttpGet()]
+    public async Task<IActionResult> GetHabits()
+    {
+        return Ok();
+    }
+}
