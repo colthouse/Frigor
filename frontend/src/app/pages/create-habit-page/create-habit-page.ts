5b3a64e321cea5845762ea20671c904c6207b315
lucyna <support@kushinada.ch>
2025-08-23 05:18:02 +0200

    gschwing biz form überarbeitet, aber nonid so banger... het no paar bugs u so i weiss xD

--- frontend/src/app/pages/create-habit-page/create-habit-page.ts
+++ frontend/src/app/pages/create-habit-page/create-habit-page.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatRadioModule } from '@angular/material/radio';
+import { FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButton } from "@angular/material/button";
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-create-habit-page',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatCardModule,
+    MatRadioModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatButton,
+    MatButtonToggleModule
+],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './create-habit-page.html',
   styleUrl: './create-habit-page.scss',
   standalone: true
 })
 export class CreateHabitPage {
+  myForm = new FormGroup({
+    name: new FormControl(''),
+    description: new FormControl(''),
+    date: new FormControl('')
+  });
+}
 
+export class ButtonToggleModeExample {
+  hideMultipleSelectionIndicator = signal(false);
+
+  toggleMultipleSelectionIndicator() {
+    this.hideMultipleSelectionIndicator.update(value => !value);
+  }
 }
