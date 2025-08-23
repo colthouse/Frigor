343434133994a00f96640947dae5520e5a212da6
FYL_GOAT <fylgoat@pm.me>
2025-08-23 20:45:28 +0200

    added some stuff for welcome and home menu links to welcome page now - vlt hani au süsch no fett inegschisse. we'll see

--- frontend/src/app/welcome/welcome.component.ts
+++ frontend/src/app/welcome/welcome.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-
+import { MenuComponent } from '../components/menu/menu.component';
+import { MatIcon } from '@angular/material/icon';
 @Component({
   selector: 'app-welcome',
   standalone: true,
   imports: [
-    MatCardModule
+    MatCardModule,
+    MenuComponent,
+    MatIcon
   ],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss'
