8cb02851ae8a5bd7db3e51ff34ad3aa3e89688ef
FYL_GOAT <fylgoat@pm.me>
2025-08-23 13:43:29 +0200

    add (feat)Get users for GodParents, no test, gabi site int

--- frontend/src/app/pages/create-habit-page/create-habit-page.ts
+++ frontend/src/app/pages/create-habit-page/create-habit-page.ts
@@ -1,4 +1,4 @@
-import { Component, signal, inject } from '@angular/core';
+import { Component, signal, inject, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatRadioModule } from '@angular/material/radio';
 import { FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
@@ -13,6 +13,9 @@ import {ChangeDetectionStrategy} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSnackBar} from '@angular/material/snack-bar';
+import { UserApi } from '../../api/services/user.api';
+import { UserModel } from '../../api/models/user.model';
+import {MatSelectModule} from '@angular/material/select';
 
 @Component({
   selector: 'app-create-habit-page',
@@ -26,14 +29,23 @@ import {MatSnackBar} from '@angular/material/snack-bar';
     MatInputModule,
     MatButtonModule,
     MatButtonToggleModule,
-    MatIconModule
+    MatIconModule,
+    MatSelectModule
 ],
   providers: [provideNativeDateAdapter()],
   templateUrl: './create-habit-page.html',
   styleUrl: './create-habit-page.scss',
   standalone: true
 })
-export class CreateHabitPage {
+export class CreateHabitPage implements OnInit{
+  
+  constructor (private userapi:UserApi) {  }
+
+  ngOnInit(): void {
+    this.userapi.getAll().subscribe(u=>this.userList=u)
+  }
+  protected userList: UserModel[]=[]
+
   myForm = new FormGroup({
     name: new FormControl(''),
     description: new FormControl(''),
@@ -58,6 +70,11 @@ export class CreateHabitPage {
       duration: 10000, // 👈 10s = 10000 ms
     });
   }
+
+  openUserList(){
+
+  }
+  
 }
 
 
