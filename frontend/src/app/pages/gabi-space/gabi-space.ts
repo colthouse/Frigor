f5870f52e4d9777d2e1afa1d3a6da6921624b388
Quiver21 <3lem3nt4ry@gmail.com>
2025-08-23 04:03:13 +0200

    neifach mal ufeglade fÃfür das ig cha material shit bruche

--- frontend/src/app/pages/gabi-space/gabi-space.ts
+++ frontend/src/app/pages/gabi-space/gabi-space.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HabitComponent } from "../../components/habit/habit.component";
 
 @Component({
   selector: 'app-gabi-space',
   standalone  : true,
-  imports: [],
+  imports: [HabitComponent],
   templateUrl: './gabi-space.html',
   styleUrl: './gabi-space.scss'
 })
