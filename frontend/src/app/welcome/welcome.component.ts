be7c15c0f50e2ff6a83cb9e1a6dbb78b2fe6bd40
FYL_GOAT <fylgoat@pm.me>
2025-08-23 22:36:52 +0200

    added loginbutton on welcom page

--- frontend/src/app/welcome/welcome.component.ts
+++ frontend/src/app/welcome/welcome.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MenuComponent } from '../components/menu/menu.component';
 import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from "@angular/material/button";
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-welcome',
   standalone: true,
   imports: [
     MatCardModule,
     MenuComponent,
-    MatIcon
-  ],
+    MatIcon,
+    MatButtonModule,
+    RouterLink
+],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss'
 })
