5c809dd93f49a5fc1a71b9628ead3302d880bcd9
lucyna <support@kushinada.ch>
2025-08-23 02:33:54 +0200

    created component create_habits Isch aues eifach mau häregrotzt.

---
+++ frontend/src/app/pages/create-habit-page/create-habit-page.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreateHabitPage } from './create-habit-page';
+
+describe('CreateHabitPage', () => {
+  let component: CreateHabitPage;
+  let fixture: ComponentFixture<CreateHabitPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CreateHabitPage]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CreateHabitPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
